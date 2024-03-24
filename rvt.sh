3421ba4aebc9feeb842acae22ab2c85f58249474
Kosong <160365589+OT4KK0SON9@users.noreply.github.com>
2024-03-25 00:10:27 +0700

    Update rm.sh

--- rvt.sh
+++ rvt.sh
@@ -5,4 +5,4 @@ cd usr/etc
 rm bash.bashrc
 cd $HOME/TERMUX-WELCOME/Revert
 mv bash.bashrc $PREFIC/etc
-python $HOME/TERMUX-WELCOME/Revert/Thanks.py
+python $HOME/TermuxSelamatDatang/Prikitiw/Thanks.py
