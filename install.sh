d8b246eb2ce7e2c7481c665cc3afc40985275898
Kosong <160365589+OT4KK0SON9@users.noreply.github.com>
2024-03-25 00:08:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@ echo ""
 echo ""
 echo "Installing Packages"
 echo "==================="
-echo "By @certified_youtuber"
+echo "By Otak Kosong"
 echo "==================="
 echo ""
 apt update
 pkg install python -y
+pkg install screenfetch
 apt upgrade
 pip install lolcat
 pkg install mpv -y
@@ -21,7 +22,7 @@ echo "Installed All Packages" |lolcat
 echo "========================="
 echo "Ready To Go.........." |lolcat
 echo "========================="
-echo "Done By @Certified_youtuber" |lolcat
+echo "Done By Otak Kosong" |lolcat
 echo "========================="
 echo "Restart Your Termux App" |lolcat
 echo "========================="
