33406ecffb76889121432c3cf9d760a337f4f913
Kosong <160365589+OT4KK0SON9@users.noreply.github.com>
2024-03-25 10:30:50 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#bin/bash/'!¡
+cp bash.bashrc $PREFIX/etc
+clear
+echo ""
+echo ""
+
+echo "========================="
+echo "Ready To Go.........." |lolcat
+echo "========================="
+echo "Done By Otak Kosong" |lolcat
+echo "========================="
+echo "Restart Your Termux App" |lolcat
+echo "========================="
+exit
