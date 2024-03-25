1c05f401d3768b25ee204900119e151b689820c0
Kosong <160365589+OT4KK0SON9@users.noreply.github.com>
2024-03-25 10:22:29 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,14 +12,14 @@ script() {
 clear
 printf "\n \e[1;92mUpdating \e[1;94mTermuxWelcome\e[1;92m ...\n\n"
 sleep 1.5
-cd ..
-rm -rf TERMUX-WELCOME
+cd
+rm -rf TermuxSelamatDatang
 git clone https://github.com/OT4KK0SON9/TermuxSelamatDatang
 cd TermuxSelamatDatang
 printf "\n\e[1;92mRestarting ...\n\e[0m"
 bash install.sh
 clear
-cd ..
+cd
 figlet DONE | lolcat
 exit
 }
